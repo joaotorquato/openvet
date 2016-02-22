ade4d965dff8166dc0498e2b0b594c6d692d8af4
Cezar Vaz <cezarvaz@gmail.com>
2016-02-22 19:27:14 -0300

    layout fixes

--- spec/features/user/user_view_all_pets_spec.rb
+++ spec/features/user/user_view_all_pets_spec.rb
@@ -15,14 +15,14 @@ feature 'User view all listed pets' do
     expect(page).to have_content 'Gato'
     expect(page).to have_content 'SRD'
     expect(page).to have_content 'F'
-    expect(page).to have_content '2016-08-01'
+    expect(page).to have_content '01/08/2016'
     expect(page).to have_xpath "//*[@*='Padme']"
     # Pet 2
     expect(page).to have_content 'Pet'
     expect(page).to have_content 'Cachorro'
     expect(page).to have_content 'Poodle'
     expect(page).to have_content 'M'
-    expect(page).to have_content '2016-07-01'
+    expect(page).to have_content '01/07/2016'
     expect(page).to have_xpath "//*[@*='Missing']"
   end
 end
